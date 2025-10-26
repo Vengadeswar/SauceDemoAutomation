9f9ff9ef995281fee9950d1f1cfde30c0e4c2b68
Vengadeswar <vengadeswar1998@gmail.com>
2025-10-26 15:42:00 +0530

    Added github actions and requirements.txt

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
-
+from selenium.webdriver.chrome.options import Options
 from Config.config import BASE_URL
 
 
 @pytest.fixture()
 def setup(request):
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(3)
     driver.get(BASE_URL)
     request.cls.driver = driver
